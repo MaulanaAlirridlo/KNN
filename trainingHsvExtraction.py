88deb3dcf10fa1436773579fb786e362ee04b6b2
MaulanaAlirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2021-11-15 18:06:44 +0700

    Add : knn using sklearn

---
+++ trainingHsvExtraction.py
@@ -0,0 +1,30 @@
+import xlsxwriter
+from os import listdir
+from os.path import isdir, isfile, splitext
+from statistics import mean
+import cv2
+
+hsvArray = []
+path = "./images/training"
+valid_images = [".jpg", ".jpeg", ".png"]
+
+hsvArray.append(["Hue", "Saturation", "Value", "Class"])
+
+for folder in listdir(path):
+    folderPath = path+"/"+folder
+    if isdir(folderPath) :
+        for file in listdir(folderPath) :
+            ext = splitext(file)[1]
+            if ext.lower() in valid_images:
+                img = cv2.imread(folderPath+"/"+file)
+                hsvImg = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+                hue = hsvImg[:, :, 0].mean()
+                saturation = hsvImg[:, :, 1].mean()
+                value = hsvImg[:, :, 2].mean()
+                hsvArray.append([hue, saturation, value, folder])
+
+workbook = xlsxwriter.Workbook('hsv.xlsx')
+worksheet = workbook.add_worksheet()
+for row, hsv in enumerate(hsvArray):
+    worksheet.write_row(row, 0, hsv)
+workbook.close()
---
+++ withSklearn.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from os import listdir
+from sklearn.neighbors import KNeighborsClassifier
+from hsvExtraction import hsvExtraction
+
+df = pd.read_excel("hsv.xlsx")
+
+x = np.array(df.iloc[:, 0:3])
+y = np.array(df['Class'])
+
+classifier = KNeighborsClassifier(n_neighbors=3)
+classifier.fit(x, y)
+
+path = './images/testing'
+for file in listdir(path) :
+    tes = classifier.predict(hsvExtraction(path+"/"+file))
+    print(tes)
