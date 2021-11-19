21cd3289231f1a9d95754749211e0a3a5d7ba057
MaulanaAlirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2021-11-19 22:55:09 +0700

    Add : image cropping process

---
+++ cropImage.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+from os import listdir, walk
+from os.path import isdir, isfile, splitext
+
+
+def croppingImage(path):
+    # Read image
+    img = cv2.imread(path)
+
+    # threshold on white
+    # Define lower and uppper limits
+    lower = np.array([200, 200, 200])
+    upper = np.array([255, 255, 255])
+
+    # Create mask to only select black
+    thresh = cv2.inRange(img, lower, upper)
+
+    # apply morphology
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
+    morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+    # invert morp image
+    mask = 255 - morph
+
+    # apply mask to image
+    result = cv2.bitwise_and(img, img, mask=mask)
+
+    cv2.imwrite(path, result)
+
+for root, subdir, file in walk("./images"):
+    for file in file :
+        croppingImage(root+"/"+file)
